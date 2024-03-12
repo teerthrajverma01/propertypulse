36299439882970da9446b28956ad2274e2f19c59
teerthrajverma01 <teerthrajverma181001@gmail.com>
2024-03-12 09:14:08 +0530

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,28 @@
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+import AuthProvider from '@/components/AuthProvider';
+import { ToastContainer } from 'react-toastify';
+import '@/assets/styles/globals.css';
+import 'react-toastify/dist/ReactToastify.css';
+
+export const metadata = {
+  title: 'PropertyPulse | Find The Perfect Rental',
+  description: 'Find your dream rental property',
+  keywords: 'rental, find rentals, find properties',
+};
+
+const MainLayout = ({ children }) => {
+  return (
+    <AuthProvider>
+      <html lang='en'>
+        <body>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+          <ToastContainer />
+        </body>
+      </html>
+    </AuthProvider>
+  );
+};
+export default MainLayout;
